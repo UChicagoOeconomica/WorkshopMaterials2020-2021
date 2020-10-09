7a8992ac1987518b57404f7a3c002236f0685f7f
AHKL <ahkandahalf@gmail.com>
2020-10-09 16:36:57 -0500

    Added some practice exercises

--- 1_IntroductoryPracticeExercises.R
+++ 1_IntroductoryPracticeExercises.R
@@ -1,1 +1,29 @@
 # Workshop 1 Practice Exercises
+
+# Find the reminder from dividing 1298460519 by 27
+
+
+# Write code that will randomly generate either TRUE or FALSE (50% of the time each)
+
+
+# Make a vector containing the integers from one to ten inclusive
+
+
+# Look up the function rev() and write code that produces the same result using the vector you just created
+
+
+# Make a vector that contains the even numbers from 15 to 35 two different ways
+
+
+# Write code that draws a number from a normal distribution with mean 25 and standard deviation 2
+
+
+# Write code to determine how likely it was that you drew that number
+
+
+# Use R to determine how many characters there are in "Oeconomica workshops occur Wednesday evenings at 6:30pm"
+
+
+# Use R to turn each word in "Oeconomica workshops occur Wednesday evenings at 6:30pm" into its own string
+
+
