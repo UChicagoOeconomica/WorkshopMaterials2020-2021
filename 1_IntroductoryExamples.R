39c1b3dd2552266a80c9bbc2e2b046eb6272c389
AHKL <ahkandahalf@gmail.com>
2020-10-09 17:25:00 -0500

    Added in examples

--- 1_IntroductoryExamples.R
+++ 1_IntroductoryExamples.R
@@ -1,2 +1,137 @@
 # Workshop 1 Example Code
 
+# Creating Variables 
+a <- 1
+
+b <- 2 + 3
+
+c <- TRUE 
+
+d <- FALSE 
+
+e = 1.879
+
+f = "Oeconomica"
+
+g = NA
+
+h = NULL 
+
+# Some basic functions
+
+typeof(f)
+
+is.numeric(a)
+
+is.integer(e)
+
+3*e
+
+3 + e
+
+45 %% 3
+
+a == b 
+
+a > b 
+
+a <= b
+
+c != (a <= b)
+
+factorial(b)
+
+# Creating vectors
+
+x <- c(1, 2, 3)
+
+y <- c(1:15)
+
+z <- 12:-12
+
+w <- seq(1, 5, by = 0.25)
+
+v <- seq(0, 5, length.out = 6)
+
+u <- rep(0, 25)
+
+t <- rep(1L, 25)
+
+s <- replicate(25, 2)
+
+r <- c(TRUE, FALSE, TRUE)
+
+q <- c("A", "a", "B")
+
+p <- c(a, b, c)
+
+# Some basic things you can do with vectors
+x[1]
+
+x[-1]
+
+w[2:4]
+
+z[c(1,9,4)]
+
+v[v > 2]
+
+y[y %% 2 == 0] <- 1
+
+length(u)
+
+u[15:length(u)] <- 0.5
+
+u + t
+
+c(x, s)
+
+c(x, s)[1:6]
+
+x + r
+
+max(x)
+
+mean(x)
+
+table(u + t)
+
+cumsum(y)
+
+# Random number generations and probability distributions
+runif(1)
+
+runif(3)
+
+runif(3, min=0, max=20)
+
+sample(1:100, 3, replace = TRUE)
+
+sample(q, 1)
+
+rnorm(3)
+
+dnorm(3)
+
+pnorm(3)
+
+qnorm(0.95)
+
+# Some basic string functions 
+a <- "Oeconomica"
+b <- "Workshop"
+ab <- paste(a, b, sep = " ")
+nchar(ab)
+substr(ab, 0, 5)
+toupper(ab)
+tolower(ab)
+strsplit(ab, " ")
+strsplit(ab, "o")
+ac <- sub("Workshop", "Meeting", ab)
+
+
+# Most important command = ?<command name>
+?floor
+?ceiling
+?round 
+
--- 1_IntroductoryPracticeExercises.R
+++ 1_IntroductoryPracticeExercises.R
@@ -3,13 +3,35 @@
 # Find the reminder from dividing 1298460519 by 27
 
 
+# Define a string variable
+
+
+# Define a boolean variable
+
+
+# Define a numeric variable 
+
+
+# Turn the three variables you just created into a vector
+
+
+# Check what kind of vector you just created
+
+
 # Write code that will randomly generate either TRUE or FALSE (50% of the time each)
 
 
 # Make a vector containing the integers from one to ten inclusive
 
 
-# Look up the function rev() and write code that produces the same result using the vector you just created
+# Look up the function rev() and write code that produces the same result using 
+# the vector you just created; save that result as another vector
+
+
+# Take the last two vectors you created and concatenate them into one longer vector
+
+
+# Take those same two vectors and add their elements together pairwise
 
 
 # Make a vector that contains the even numbers from 15 to 35 two different ways
@@ -18,7 +40,10 @@
 # Write code that draws a number from a normal distribution with mean 25 and standard deviation 2
 
 
-# Write code to determine how likely it was that you drew that number
+# Determine how likely it was that you drew that number
+
+
+# Draw three random integers from 0 to 100
 
 
 # Use R to determine how many characters there are in "Oeconomica workshops occur Wednesday evenings at 6:30pm"
