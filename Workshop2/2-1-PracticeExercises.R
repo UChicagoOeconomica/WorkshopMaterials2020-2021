4784a8880dc19f4f4c4f95775a1d248c37450faf
AHKL <ahkandahalf@gmail.com>
2020-10-19 10:45:00 -0500

    added some practice exercises

--- Workshop2/2-1-PracticeExercises.R
+++ Workshop2/2-1-PracticeExercises.R
@@ -1,3 +1,36 @@
 # Practice Exercises for Workshop 2 Part 1
+# Note, not all of these exercises require the use of R. It may be beneficial 
+# for you to figure out how to do them in R, but it is not necessary
 
-# Take 
+# A biased coin lands heads with probability 2/3. The coin is tossed 3 times. 
+# Given that there was at least one head in the 3 tosses, what is the 
+# probability that there were at least two heads? Use your answer to the 
+# previous question to find the probability that there was exactly one head in 
+# the three tosses given that there was at least one head in the three tosses.
+# Now, what if the coin was actually biased with probability 0.68 to land heads, 
+# how would your answer to the two questions change?
+
+
+
+# Observe how the sample mean of larger and larger samples come closer to the 
+# mean of the chi square distribution with three degrees of freedom
+# by computing the sample mean of random samples of 5, 10, 50, 100, 1000, 
+# 10000, and 100000
+
+
+# Observe how the sample mean of larger and larger samples of the exponential 
+# distribution come closer to the expectation by computing the sample mean of
+# random samples of 5, 10, 50, 100, 1000, 10000, and 100000
+
+
+# Which sequence of sample means converged faster? That of the chi-squared 
+# distribution or the exponential distribution
+
+# Why might that be true?
+
+
+# Take random samples of 100, 500, and 1000 from the t distribution with 5 
+# degrees of freedom and from the normal distribution. Compute the difference 
+# between the maximum values for each sample size. What does this show? Why does 
+# this matter?
+
