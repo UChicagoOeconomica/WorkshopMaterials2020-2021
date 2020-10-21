7d8d0180d8cac9f686dc3976e088384a7a79c3b0
AHKL <ahkandahalf@gmail.com>
2020-10-21 14:13:31 -0500

    Added exercises from Azize

--- Workshop2/2-1-PracticeExercises.R
+++ Workshop2/2-1-PracticeExercises.R
@@ -2,6 +2,7 @@
 # Note, not all of these exercises require the use of R. It may be beneficial 
 # for you to figure out how to do them in R, but it is not necessary
 
+
 # A biased coin lands heads with probability 2/3. The coin is tossed 3 times. 
 # Given that there was at least one head in the 3 tosses, what is the 
 # probability that there were at least two heads? Use your answer to the 
@@ -10,6 +11,24 @@
 # Now, what if the coin was actually biased with probability 0.68 to land heads, 
 # how would your answer to the two questions change?
 
+
+# Show that if E[Y|X] = b, where b is some constant, then Y is mean independent 
+# of X; (X and Y are random variables)
+
+
+# Give an example of two variables X, Y, where E[X|Y] = E[X] (i.e. they are mean
+# independent) but X and Y are actually not independent
+
+# Prove that the covariance of two mean independent random variables is equal to
+# zero
+
+# Show that for a sample of independent random variables X_1, ..., X_n that all 
+# have the same distribution with mean a and variance b^2, Var(S) = b^2/n, where
+# S is the average of all X_i (i.e. S = 1/n(X_1 + ... + X_n))
+
+# Prove that Var(X - Y) = Var(x) + Var(Y) - 2Cov(X, Y)
+
+# Prove Cov(a + bX, c + dY) = bdCov(X, Y)
 
 
 # Observe how the sample mean of larger and larger samples come closer to the 
