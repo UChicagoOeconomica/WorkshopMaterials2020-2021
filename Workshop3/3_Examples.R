52a1b2fc68f3109750793c3cfda765b6c866c1ce
AHKL <ahkandahalf@gmail.com>
2020-11-09 22:14:16 -0600

    Began adding exercises and sectioning examples; added datasets for W3

---
+++ Workshop3/3_Examples.R
@@ -0,0 +1,28 @@
+# Examples for Workshop 3
+
+# Start by installing and loading your packages!
+install.packages('tidyverse')
+library(tidyverse)
+
+# R (and some of its packages) have some data already
+data("txhousing")
+# For a dataframe native to R, find out more about the dataset by running
+?txhousing
+
+# Most of the time, you will need to load your own data in using the correct
+# command for that datatype 
+cps_df <- read.csv("Workshop3/cps.csv")
+
+# Glancing at your data 
+
+
+# Finding Summary Statistics
+
+
+# Subsetting the Dataset
+
+
+# Building New Variables 
+
+
+
--- Workshop3/3_PracticeExercises.R
+++ Workshop3/3_PracticeExercises.R
@@ -2,5 +2,20 @@
 
 # For many exercises, there are multiple ways to find a solution
 
+# Load elections.tab 
+# (Hint: ".tab" refers to a tab delimited file)
 
+# This dataset contains two variables from elections for House races between 
+# 1948 and 1998. It contains two variables "difdemshare" (which is the percentage
+# of votes the Democratic candidate won minus the percentage that the Republican
+# candidate won) and "demsharenext" (which is the percentage of votes that the
+# Democratic candiate won in the next election)
 
+# Find the mean and standard deviation of both variables 
+
+# Construct an estimate for P(Dem won next election | Dem lost)
+# (Hint: the expectation of an indicator variable is the probability that the 
+# variable is one)
+
+# Find the mean, standard deviation, and quintiles of demsharenext for elections 
+# where the margin of victory was within 5%
