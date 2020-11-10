6a8702b9e7010ec79808fffe425c29cbf48d13d2
AHKL <ahkandahalf@gmail.com>
2020-11-09 22:38:48 -0600

    a couple examples added

--- Workshop3/3_Examples.R
+++ Workshop3/3_Examples.R
@@ -14,12 +14,22 @@ data("txhousing")
 cps_df <- read.csv("Workshop3/cps.csv")
 
 # Glancing at your data 
-
+view(txhousing)
+head(cps_df)
 
 # Finding Summary Statistics
-
+summarise(cps_df, avg_wage=mean(incwage), med_age=median(age))
+tx_housing_summ_df <- 
 
 # Subsetting the Dataset
+# Keeping or removing variables (2 ways):
+cps_few_vars1 <- select(cps_df, incwage, age, sex)
+cps_few_vars2 <- select(cps_df, -vetstat, -hispan, -ind)
+cps_few_vars3 <- cps_df[,c(1,4,5)]
+cps_few_vars4 <- cps_df[, -c(8, 9, 12)]
+# Keeping or removing rows
+
+
 
 
 # Building New Variables 
