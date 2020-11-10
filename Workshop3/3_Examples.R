e623d35f511f21cca097080a484a831693a47830
AHKL <ahkandahalf@gmail.com>
2020-11-10 10:17:33 -0600

    adding examples for dealing with NAs and getting diff summ stats

--- Workshop3/3_Examples.R
+++ Workshop3/3_Examples.R
@@ -18,17 +18,57 @@ view(txhousing)
 head(cps_df)
 
 # Finding Summary Statistics
+
+# You can use any function that you can use on a vector!
+mean(txhousing$sales)
+sd(txhousing$sales)
+# Be careful about NA values! Instead you may want to use 
+mean(txhousing$sales, na.rm = TRUE)
+
+# Side note on NA values:
+# Removing them completely
+txhousing_nonas1 <- na.omit(txhousing)
+txhousing_nonas2 <- txhousing[complete.cases(txhousing),]
+txhousing_nonas3 <- na.exclude(txhousing)
+# Finding them
+which(is.na(txhousing$volume))
+sum(is.na(txhousing))
+colSums(is.na(txhousing))
+# Replacing NAs
+txhousing_0_for_NA <- txhousing
+txhousing_0_for_NA[is.na(txhousing_0_for_NA)] <- 0
+txhousing_rep_NAs <- replace_na(txhousing, list(sales=0,volume=1,
+                                                listings=0,inventory=1))
+
+# Getting several at once
+lapply(txhousing, mean, na.rm=TRUE) #Returns a list
+sapply(txhousing, mean, na.rm=TRUE) #Returns a vector
 summarise(cps_df, avg_wage=mean(incwage), med_age=median(age))
-tx_housing_summ_df <- 
+# What if you want summary statistics by a particular variable?
+tx_housing_city_groups <- group_by(txhousing, city)
+tx_housing_summ_df_by_city <- summarise(tx_housing_city_groups, 
+                                        avg_volume=mean(volume), 
+                                        avg_listings=mean(listings),
+                                        avg_sales=mean(sales))
+tx_housing_summ_df_by_city_no_NAs <- summarise(tx_housing_city_groups, 
+                                        avg_volume=mean(volume,na.rm=TRUE), 
+                                        avg_listings=mean(listings, na.rm=TRUE),
+                                        avg_sales=mean(sales, na.rm=TRUE))
+tapply(cps_df$incwage, cps_df$educ, mean)
 
 # Subsetting the Dataset
 # Keeping or removing variables (2 ways):
-cps_few_vars1 <- select(cps_df, incwage, age, sex)
-cps_few_vars2 <- select(cps_df, -vetstat, -hispan, -ind)
-cps_few_vars3 <- cps_df[,c(1,4,5)]
-cps_few_vars4 <- cps_df[, -c(8, 9, 12)]
+cps_fewer_vars1 <- select(cps_df, incwage, age, sex)
+cps_fewer_vars2 <- select(cps_df, -vetstat, -hispan, -ind)
+cps_fewer_vars3 <- cps_df[,c(1,4,5)]
+cps_fewer_vars4 <- cps_df[, -c(8, 9, 12)]
 # Keeping or removing rows
+txhousingfirstrows <- txhousing[c(1:100),]
+txhousingnofirstrows <- txhousing[-c(1:100),]
+txhousingsample <- sample_n(txhousing, 300, replace = FALSE)
 
+
+# Dealing with NA values 
 
 
 
