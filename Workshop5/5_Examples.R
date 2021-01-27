cd8cfb767768d4f273dd3a66dbbcb70ec13dd7de
AHKL <ahkandahalf@gmail.com>
2021-01-27 12:58:01 -0600

    Setting up files for workshop 5

---
+++ Workshop5/5_Examples.R
@@ -0,0 +1,1 @@
+# Examples for Workshop 5
---
+++ Workshop5/5_PracticeExercises.R
@@ -0,0 +1,7 @@
+# Practice Exercises for Workshop 5
+
+# Save all your output to your output folder; data that is not native to R can 
+# be found in the data folder for Workshop 5
+
+
+# Using the data from cps.csv in your data folder
