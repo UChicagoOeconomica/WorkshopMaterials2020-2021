2a4204b5dca2365cffc1665edcf6f0b4c3a0e20c
AHKL <ahkandahalf@gmail.com>
2021-01-27 17:40:07 -0600

    Filling in Examples and Creating Practice Exercises

--- Workshop5/5_Examples.R
+++ Workshop5/5_Examples.R
@@ -1,1 +1,118 @@
 # Examples for Workshop 5
+# Packages to load!
+library(tidyverse)
+library(stargazer) 
+# if you do not have the stargazer package, you will need to install it
+
+
+
+## An example
+tfp_df <- read.csv("Workshop5/Data/TFP.csv")
+# it's always helpful to glance over the data first:
+summary(tfp_df)
+# your experience plotting things from Workshop 4 can be helpful!
+# for instance, we can look at the relationship between output and labor
+ggplot(tfp_df, aes(x=Labor.input, y = Value.added.output)) + 
+  geom_point() + 
+  labs(x="Labor", y = "Output", 
+       title = "Relationship Between Labor and output") + 
+  theme_bw()
+
+# note that ggplot already allows us to easily add a line to represent the most
+# basic regression
+ggplot(tfp_df, aes(x=Labor.input, y = Value.added.output)) + 
+  geom_point() + 
+  labs(x="Labor", y = "Output", 
+       title = "Relationship Between Labor and output") + 
+  theme_bw() + 
+  geom_smooth(method = lm)
+# This just represents the regression of y on x and a constant, i.e. no controls
+
+# We can find this line via regression using the lm command
+lm(formula = Value.added.output ~ Labor.input, data = tfp_df)
+# or even better
+lab_output_mod1 <- lm(formula = Value.added.output ~ Labor.input, data = tfp_df)
+summary(lab_output_mod1)
+
+lab_output_mod2 <- lm(formula = Value.added.output ~ Labor.input + 
+                        Capital.services, data = tfp_df)
+summary(lab_output_mod2)
+
+stargazer(lab_output_mod1, lab_output_mod2,
+          align=TRUE, no.space=TRUE, #omit.stat="f",
+          dep.var.labels = c("Value Added Output"),
+          covariate.labels = c("Labor Input",
+                               "Capital Services"),
+          title="Value Added Output Regressions",
+          digits=4)
+# the default for stagazer is to give you output in LaTeX! 
+
+stargazer(lab_output_mod1, lab_output_mod2,
+          align=TRUE, no.space=TRUE, omit.stat="f",
+          dep.var.labels = c("Value Added Output"),
+          covariate.labels = c("Labor Input",
+                               "Capital Services"),
+          title="Value Added Output Regressions",
+          type = "text",
+          digits=4)
+
+# You can directly save to a file using Stargazer
+stargazer(lab_output_mod1, lab_output_mod2,
+          align=TRUE, no.space=TRUE, omit.stat="f",
+          dep.var.labels = c("Value Added Output"),
+          covariate.labels = c("Labor Input",
+                               "Capital Services"),
+          title="Value Added Output Regressions",
+          digits=4,
+          out = "Workshop5/Output/exampletable1.txt")
+
+
+cd_output_mod <- lm(formula = log(Value.added.output) ~ log(Labor.input) + 
+                      log(Capital.services), data = tfp_df)
+
+
+
+## Another Example
+cps_df <- read.csv("Workshop5/Data/CPS.csv")
+
+Post_HS_degrees <- unique(cps_df$educ)[c(1, 3, 5, 7, 9, 16)]
+cps_df$has_degree <- ifelse(cps_df$educ %in% Post_HS_degrees, TRUE, FALSE)
+
+# Using linear regression we can find the difference in having a post-high
+# school degree versus not having one
+deg_impact_mod1 <- lm(data = cps_df, incwage ~ has_degree)
+
+be_has_degree <- mean(cps_df$incwage[cps_df$has_degree])
+be_no_degree <- mean(cps_df$incwage[!cps_df$has_degree])
+
+lm(data = cps_df, incwage ~ has_degree, subset = (sex == "Male"))
+lm(data = cps_df, incwage ~ has_degree, subset = (sex == "Female"))
+
+deg_impact_mod2 <- lm(data = cps_df, incwage ~ has_degree + sex)
+
+be_fm_no_degree <- mean(cps_df$incwage[(!cps_df$has_degree) & 
+                                         cps_df$sex=="Female"])
+
+mean(cps_df$incwage) - 39540.6 * mean(cps_df$has_degree) - 24141.3 * 
+  (length(cps_df$incwage[cps_df$sex=="Male"])/length(cps_df$incwage))
+
+deg_impact_mod3 <- lm(data = cps_df, incwage ~ has_degree * sex)
+
+# lm(data = cps_df, incwage ~ has_degree:sex + has_degree + sex)
+
+deg_impact_mod4 <- lm(data = cps_df, incwage ~ has_degree * sex + age)
+
+deg_impact_mod5 <- lm(data = cps_df, incwage ~ has_degree + sex + age)
+
+educ_impact_mod1 <- lm(data = cps_df, incwage ~ factor(educ))
+
+educ_impact_mod2 <- lm(data = cps_df, incwage ~ factor(educ) + sex + age)
+
+stargazer(deg_impact_mod1, deg_impact_mod2, deg_impact_mod3, 
+          deg_impact_mod4, deg_impact_mod5,
+          align=TRUE, no.space=TRUE, omit.stat="f",
+          dep.var.labels = c("Value Added Output"),
+          covariate.labels = c("Has Degree",
+                               "Male", "Age", "Has Degree * Male"),
+          title="Value Added Output Regressions",
+          digits = 4, out = "Workshop5/Output/exampletable2.txt")
--- Workshop5/5_PracticeExercises.R
+++ Workshop5/5_PracticeExercises.R
@@ -4,4 +4,70 @@
 # be found in the data folder for Workshop 5
 
 
-# Using the data from cps.csv in your data folder
+# Using the data from cps.csv in your data folder do the following:
+# (Note: you may want to spend a bit of time exploring the data to understand it
+# first)
+
+# Use linear regression to compute the binning estimator of average wage income
+# for women in Florida 
+# (Hint 1: the binning estimator is the sample mean of that bin)
+# (Hint 2: you may want to construct indicator variables)
+
+# Regress the log of wage income on an indicator for gender, age, age squared, 
+# race, and a full set of dummy variables for reported level of education
+
+# What is the coefficient on the gender indicator? How do you interpret this
+# coefficient
+
+# Run the same regression but with interactions between the gender indicator
+# and both the race and education indicators. What do the coefficients on the 
+# interaction terms between race and gender imply? Are they similar?
+
+
+# Using the data from voena.csv in your data folder, do the following:
+# (A note about this data: This data is from the paper "Widows Land Rights
+# and Agricultural Investment" by Dillon and Voena, which explores the effect 
+# of widow's inheritance rights on agricultural investment in Zambia)
+# some notes on variables:
+#     noninh = 1 denotes non-inheritance
+#     fert = fertilization in kilograms
+#     hh_couple = 1 denotes married households
+
+# Regress fertilization on inheritance rights alone for just married households.
+# How would you interpret this coefficient?
+
+# Regress fertilization on inheritance rights controlling for district for just
+# married households. 
+# How would you interpret the coefficient on inheritance rights now? What is a
+# reason that the coefficient has changed from your previous regression?
+
+# How would you use the data you have to enrich the regression? 
+# (Hint: This can be by running a different specification or by providing
+# additional context for your results)
+
+# Repeat both regressions for non-coupled households. Why would these results
+# be useful in arguing that widow's property rights affect agricultural
+# investment?
+
+# Make a table to represent these regressions
+
+# Using the data in txhousing (a dataset in R), use regressions to come up with
+# a model that looks at the determinants of median sale price in a given month
+# (Hint: Variables related to time are helpful for understanding causality)
+
+# Using the data in CO2 (a dataset in R), find the impact of being chilled on
+# CO2 uptake
+
+
+# Using the data in ChickWeight (a dataset in R), figure out which diet is the 
+# best for the chicks and which is the worst (assuming that "best" means that
+# the chick gained weight)
+
+
+# Using the storms data (a dataset in R), figure out the characteristics of a 
+# storm that make them last longer
+
+
+
+# Using the diamonds data (a dataset in R), use regression to create a model of
+# diamond price
